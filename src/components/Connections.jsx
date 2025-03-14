2ea393d981d4eac4dbdc2d5239b625b332621754
SulavSinghal <sulavsinghal01@gmail.com>
2025-03-14 12:20:03 +0530

    Requests Page

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -29,9 +29,9 @@ const Connections = () =>{
 
         {
             connections.map((connection) => {
-                const {firstName, lastName, photoUrl, age, gender, about} = connection
+                const {_id,firstName, lastName, photoUrl, age, gender, about} = connection
                 return (
-                <div className="flex m-4 p-4 border rounded-lg bg-base-300 w-1/2 mx-auto">
+                <div key ={_id} className="flex m-4 p-4 border rounded-lg bg-base-300 w-1/2 mx-auto">
                     <div><img alt="photo" className= "w-20 h-20 rounded-full"src={photoUrl}></img></div>
                     <div className="text-left mx-4">
                     <h2 className="font-bold text-xl">{firstName + " " + lastName}</h2>
